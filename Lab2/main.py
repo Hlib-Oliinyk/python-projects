8ebcabd4f9b6ec9b1f9f7d2fe3dc4f239086cb18
Олійник Гліб <ipz242_ogi@student.ztu.edu.ua>
2025-09-26 17:51:41 +0300

    Виконано завдання 11 та 12

--- Lab2/main.py
+++ Lab2/main.py
@@ -75,6 +75,103 @@ def task10(n):
         num = (k ** 2) + 1
     print(f"Перше число яке більше за {n}, це {num}.")
 
+def task11(D, M):
+    if M == 1:
+        if D <= 19:
+            zodiac = "Козеріг"
+        elif D <= 31:
+            zodiac = "Водолій"
+
+    elif M == 2:
+        if D <= 18:
+            zodiac = "Водолій"
+        elif D <= 29:
+            zodiac = "Риби"
+
+    elif M == 3:
+        if D <= 20:
+            zodiac = "Риби"
+        elif D <= 31:
+            zodiac = "Овен"
+
+    elif M == 4:
+        if D <= 19:
+            zodiac = "Овен"
+        elif D <= 30:
+            zodiac = "Телець"
+
+    elif M == 5:
+        if D <= 20:
+            zodiac = "Телець"
+        elif D <= 31:
+            zodiac = "Близнюки"
+
+    elif M == 6:
+        if D <= 21:
+            zodiac = "Близнюки"
+        elif D <= 30:
+            zodiac = "Рак"
+
+    elif M == 7:
+        if D <= 22:
+            zodiac = "Рак"
+        elif D <= 31:
+            zodiac = "Лев"
+
+    elif M == 8:
+        if D <= 22:
+            zodiac = "Лев"
+        elif D <= 31:
+            zodiac = "Діва"
+
+    elif M == 9:
+        if D <= 22:
+            zodiac = "Діва"
+        elif D <= 30:
+            zodiac = "Терези"
+
+    elif M == 10:
+        if D <= 22:
+            zodiac = "Терези"
+        elif D <= 31:
+            zodiac = "Скорпіон"
+
+    elif M == 11:
+        if D <= 22:
+            zodiac = "Скорпіон"
+        elif D <= 30:
+            zodiac = "Стрілець"
+
+    elif M == 12:
+        if D <= 21:
+            zodiac = "Стрілець"
+        elif D <= 31:
+            zodiac = "Козеріг"
+
+    print(f"Дата {D}.{M} відповідає знаку зодіаку - {zodiac}")
+
+def task12(n, value):
+    match n:
+        case 1:
+            n = "кілограм"
+            kg = 1.0
+        case 2:
+            n = "міліграм"
+            kg = 0.000001
+        case 3:
+            n = "грам"
+            kg = 0.001
+        case 4:
+            n = "тонна"
+            kg = 1000.0
+        case 5:
+            n = "центнер"
+            kg = 100.0
+
+    in_kg = value * kg
+    print(f"Початкова маса: {value} {n}")
+    print(f"Маса в кілограмах: {in_kg} кг")
+
 print("Завдання 1")
 task1()
 
@@ -193,5 +290,35 @@ while True:
             continue
         task10(n)
         break
+    except ValueError:
+        print("Помилка введення")
+
+print("Завдання 11")
+while True:
+    try:
+        D = int(input("Введіть день D "))
+        M = int(input("Введіть місяць M "))
+        if M < 0 or M >= 12 or D < 0 or D >= 31:
+            print("Помилка введення дня 1-31 або місяця 1-12")
+            continue
+        task11(D, M)
+        break
+    except ValueError:
+        print("Помилка введення")
+
+print("Завдання 12")
+while True:
+    try:
+        print("Одиниці маси: 1 - кг, 2 - мг, 3 - г, 4 - т, 5 - ц")
+        num = int(input("Введіть номер одиниці маси "))
+        mass = float(input("Введіть масу тіла "))
+
+        if 1 <= num <= 5 and mass >= 0:
+            task12(num, mass)
+            break
+        elif num < 1 or num > 5:
+            print("Номер має бути в діапазоні від 1 до 5")
+        elif mass < 0:
+            print("Маса не може бути від'ємною")
     except ValueError:
         print("Помилка введення")
