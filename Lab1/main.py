bf862427003e71ee736953119524c52541f2c7cf
Олійник Гліб <ipz242_ogi@student.ztu.edu.ua>
2025-09-26 15:30:18 +0300

    Виконано завдання 4 та 5

--- Lab1/main.py
+++ Lab1/main.py
@@ -61,9 +61,32 @@ def task3(n):
             pare.append(i)
     print(f"Кількість елементів у списку - {len(n)}", f"\nПарні елементи списку - {pare}")
 
+def task4(n):
+    saved = []
+    saved.append(n[1]), saved.append(n[4])
+    n[1] = saved[1]
+    n[4] = saved[0]
+    print(f"Масив після перестановки 2 і 5 елемту - {n}")
+
+def task5():
+    while True:
+        name = str(input(""))
+        if name.replace(" ", "").isalpha():
+            print(f"Лабораторну виконав {name}\nВисковки:\n"
+            f"1. В ході виконання лабораторної роботи я ознайомився з алгоритмами послідовної (лінійної) структури, з процедурами запуску програм, які реалізують ці алгоритми на мові Python."
+            f"\n2. Познайомився з інтегрованим середовищем розробки – integrated development environment (IDLE).")
+            break
+        else:
+            print("Ви ввели не текст")
+
+
 # Завдання 1
 numbers1 = task1()
 # Завдання 2
 numbers2 = task2(numbers1)
 #Завдання 3
 task3(numbers2)
+#Завдання 4
+task4(numbers2)
+#Завдання 5
+task5()
